28f000caa4931831f490a5834875b9aa79f742af
John Cartwright <johncartwright302@gmail.com>
2023-07-28 09:24:58 +1000

    Create index.php
    Creating the index.php file.

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Australian Radio and TV</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <h1>Radio and TV</h1>
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <section>
+      <h2>Section 1</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque massa sit amet facilisis fermentum.</p>
+    </section>
+    <section>
+      <h4>Sidebar</h4>
+      <p>Phasellus sed mi ac purus fringilla dapibus. Fusce ultrices facilisis odio at consequat.</p>
+    </section>
+  </main>
+  <footer>
+    <p>&copy; <?php echo date('Y'); ?> Your Site Name. All rights reserved.</p>
+  </footer>
+</body>
+</html>
